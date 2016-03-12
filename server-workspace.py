67b9425c0a311cfc0338db6cab5855763f3a7fed
jaycode <teguhwpurwanto@gmail.com>
2016-03-12 15:26:48 +0700

    refactor: Remove workspace code and add environment variables to config

--- server-workspace.py
+++
@@ -1,8 +0,0 @@
-import tornado.ioloop
-import tornado.web
-import sockjs.tornado
-from app_workspace import app
-
-if __name__ == '__main__':
-    app.listen(49152)
-    tornado.ioloop.IOLoop.instance().start()
